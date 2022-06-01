2a2b8538aa246c18e25c585931dba939f12c987f
Jose305 <jaruiz17@espe.edu.ec>
2022-06-01 08:25:34 -0500

    Version 2

--- [NRC_6278]_JoseRuizTaller6.py
+++ [NRC_6278]_JoseRuizTaller6.py
@@ -2,18 +2,26 @@
         ID: L00380361
         Materia: Inteligencia Artificial
 """
-""" En este programa se trata de la búsqueda primero en amplitud. Recorre sistemáticamente el gráfico, 
-    nivel por nivel, y crea un árbol BFS en el camino. La búsqueda en amplitud se puede utilizar para resolver muchos problemas, 
-    como encontrar el camino más corto entre dos nodos. Por lo tanto, determina el nivel de cada nodo e incluso resolver 
-    juegos de rompecabezas y laberintos.
+""" 
+____________________________________________________________________________________________________________________________
+En este programa se trata de la búsqueda primero en amplitud. Recorre sistemáticamente el gráfico, 
+nivel por nivel, y crea un árbol BFS en el camino. La búsqueda en amplitud se puede utilizar para resolver muchos problemas, 
+como encontrar el camino más corto entre dos nodos. Por lo tanto, determina el nivel de cada nodo e incluso resolver 
+juegos de rompecabezas y laberintos.
+____________________________________________________________________________________________________________________________
+
 """
+
 #Se importar la libreria Queue clase correspondiente del queue módulo.
 from queue import Queue
 
 """
+______________________________________________________________________
 La clase Esquema contiene una representación gráfica, 
 en este caso una matriz de adyacencia, 
 y todos los métodos que pueda necesitar cuando trabaje con gráficos.
+______________________________________________________________________
+
 """ 
 
 class Esquema:
@@ -53,57 +61,27 @@ ________________________________________________________________________________
     
     #Añadiendo borde al grafo
     #Se agrega los parametros en la funcion agregando_arista
-    def agregar_arista(punto, union1, union2, weight=1):
+    def agregar_arista(punto, nodo1, nodo2, peso=1):
         
-        """
-        Añadiendo Grafos
-        
-        Se pasa una llave como identificador para su posterior ingreso de valor
-
-        Parametros
-        ----------
-        node1: int
-            valor del primer nodo 
-        node2: int
-            valor del segundo nodo
-        peso: int, valor por defecto 1
-            El peso de los grafos poderados
-
-        Si dirigido es falso devolvera True para ingresar como llave el nodo2 y su valor el nodo 1
-
-        Retorna
-        -------
-        Nada
-        """
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        punto.m_aproximidad[union1].add((union2, weight))
+        """ Añadiendo Grafos
+        Se identifica la llave como identificador para su ingreso de valores.
+        Los parámetros son:
+        (nodo1: int) ==> correspondiente al primer valor del nodo 
+        (node2: int) ==> correspondiente al segundo valor del nodo
+        (peso: int) ==> inicializando el valor a 1 
+        La condicion de los nodo es que si es falso retornara verdadero para agregar
+        la llave del nodo 2 y por el valor del nodo 1
+        Si dirigido es falso devolvera True para ingresar como llave el nodo2 y su valor el nodo 1 """        
+       
+        punto.m_agregando_lista[nodo1].add((nodo2, peso))
         """Se ubica una condición en caso que no agregue ninguna arista se matendra de forma indirecta con los valores."""
-        if not punto.m_determinado:
+        if not punto.m_dirigido:
             """Insertara la siguiente arista sucesora"""
-            punto.m_aproximidad[union2].add((union1, weight))
+            punto.m_agregando_lista[nodo2].add((nodo1, peso))
     """Imprimira la representacion grafico de los nodos."""
     def print_aproximidad(self):
-        for key in self.m_aproximidad.keys():
-            print("union", key, ": ", self.m_aproximidad[key])
+        for key in self.m_agregando_lista.keys():
+            print("union", key, ": ", self.m_agregando_lista[key])
     """La función de la interseccion se encargara de imprimir el recorrido BFS, desde un vertice de origen es decir el inicio del nodo.
     para recorrer los vertices."""
     def interseccion(punto, inicio_vinculo):
@@ -120,7 +98,7 @@ ________________________________________________________________________________
             print(union_vigente, end = " ")
             """Se obtiene todos los vertices adyacentes de la cola, si un vertice adyacente no ha sido visitado,
             entonces lo ira marcando y poniendolo en cola."""
-            for (proxima_union, weight) in punto.m_aproximidad[union_vigente]:
+            for (proxima_union, weight) in punto.m_agregando_lista[union_vigente]:
                 if proxima_union not in llegadas:
                     cola.put(proxima_union)
                     llegadas.add(proxima_union)
